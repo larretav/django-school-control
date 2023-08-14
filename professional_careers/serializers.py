96393e7c6cc089c11e9b0c2ed1a17a729c6829c1
Jesus <yakasawua@hotmail.com>
2023-08-13 23:49:00 -0600

    The SchoolSubject model was modified to add the careers; modified SchoolSubject and ProfessionalCareer serializers; added the __str__ of SchoolSubject and ProfessionalCareer

--- professional_careers/serializers.py
+++ professional_careers/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import ProfessionalCareer
+from school_subjects.serializers import SchoolSubjectCarrersSerializer
 
 class ProfessionalCareerSerializer(serializers.ModelSerializer):
-
+    school_subjects = SchoolSubjectCarrersSerializer(many=True, source='school_subject_carrer')
     class Meta:
         model = ProfessionalCareer
         fields = '__all__'
--- school_subjects/models.py
+++ school_subjects/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+from professional_careers.models import ProfessionalCareer
 
 # Create your models here.
-
+# materias
 class SchoolSubject(TimeStampedModel):
     name = models.CharField(max_length=100, null=True, blank=True, verbose_name='Nombre de la materia')
     semester = models.PositiveIntegerField(null=True, blank=True, verbose_name='Semestre al cual pertenece la materia')
+    career = models.ForeignKey(ProfessionalCareer, related_name='school_subject_carrer', blank=True, null=True, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '%s - %s - %s' % (self.name, self.semester, self.career.name)
