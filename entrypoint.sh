6fff32c1976232c65f8be597f2897e15101eb534
larretav <larreta_vzla@hotmail.com>
2023-08-18 12:16:58 -0700

    Configuración para deploy 38 - Docker 32

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py wait_for_db --settings=django_school_control.settings
 python manage.py migrate --settings=django_school_control.settings
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production django_school_control.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn --env DJANGO_SETTINGS_MODULE=django_school_control.settings django_school_control.wsgi:application --bind 0.0.0.0:$PORT
